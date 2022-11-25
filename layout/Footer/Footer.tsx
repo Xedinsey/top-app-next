03562e58f986f4dc9a04fccff9cb88ca0d5e2051
Max <gameeter@gmail.com>
2022-11-25 23:19:02 +0300

    09-008 Layout.module.css верстка сетки

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {FooterProps} from "./Footer.props";
-import styles from './Footer.module .css';
+import styles from './Footer.module.css';
 import cn from "classnames";
+import {format} from "date-fns";
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className,...props}: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>OwlTop © {format(new Date(), 'yyyy')} Все права защищены</div>
+            <a className={styles.link} href="#" target="_blank">Пользовательское соглашение</a>
+            <a className={styles.link} href="#" target="_blank">Политика конфиденциальности</a>
+        </footer>
     );
 };
--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SidebarProps} from "./Sidebar.props";
-import styles from './Sidebar.module .css';
+import styles from './Sidebar.module.css';
 import cn from "classnames";
 
 export const Sidebar = ({...props}: SidebarProps): JSX.Element => {
